57ea3c1f36172aa7dc4c37ac4ee88b00304f9655
itsliseshd <lise.souhard54@orange.fr>
2018-09-27 09:19:30 +0000

    Création du serveur du Raspberry-Walking-dead

--- server.js
+++ server.js
@@ -1,15 +1,23 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const os = require("os");
+const mustacheExpress = require('mustache-express');
+app.engine('mustache', mustacheExpress());
+app.set('view engine', 'mustache');
+app.set('views', __dirname + '/views');
+app.use(express.static('public'));
 app.get('/', (request, response) => {
- response.send('Requête reçue...')
+ response.render('index');
 })
 app.get('/hello/:name', (request, response) => {
- response.send('Hello, '+request.params.name+'!');
+ response.render('Hello',{name: request.params.name});
 })
+app.get('/pooc', (request, response) =>{
+response.render('endpoint');})
 app.listen(port, (err) => {
  if (err) {
  return console.log('Erreur du serveur : ', err)
  }
- console.log('Le serveur écoute sur le port'+port+'Rendez vous sur http://localhost:'+port
-);})
+ console.log('Le serveur écoute sur le port'+port+'\nRendez vous sur http://'+os.hostname()+'.local:'+port);
+;})
